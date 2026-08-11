80c6420895041661235c7143d4f3da2cdbc74ef0
L. Milenov <lusi_milenov@abv.bg>
2026-08-11 14:05:39 +0300

    End of section 16

--- API/Data/AppDbContext.cs
+++ API/Data/AppDbContext.cs
@@ -1,22 +1,29 @@
 using System;
 using API.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace API.Data
 {
-    public class AppDbContext(DbContextOptions options) : DbContext(options)
+    public class AppDbContext(DbContextOptions options) : IdentityDbContext<AppUser>(options)
     {
-        public DbSet<AppUser> Users { get; set; }
         public DbSet<Member> Members { get; set; }
         public DbSet<Photo> Photos { get; set; }
         public DbSet<MemberLike> Likes { get; set; }
-
         public DbSet<Message> Messages { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<IdentityRole>()
+                .HasData(
+                    new IdentityRole{Id = "member-id", ConcurrencyStamp = "a" , Name = "Member", NormalizedName = "MEMBER"},
+                    new IdentityRole{Id = "moderator-id", ConcurrencyStamp = "b" , Name = "Moderator", NormalizedName = "MODERATOR"},
+                    new IdentityRole{Id = "admin-id", ConcurrencyStamp = "c" , Name = "Admin", NormalizedName = "ADMIN"}
+                );
 
             modelBuilder.Entity<Message>()
                 .HasOne(x => x.Recipient)
--- API/Interfaces/IMemberRepository.cs
+++ API/Interfaces/IMemberRepository.cs
@@ -11,7 +11,7 @@ namespace API.Interfaces
         Task<PaginatedResult<Member>> GetMembersAsync(MemberParams memberParams);
         Task<Member?> GetMemberByIdAsync(string id);
         Task<IReadOnlyList<Photo>> GetPhotosForMemberAsync(string memberId);
-        Task<Member> GetMemberForUpdate(string id);
+        Task<Member?> GetMemberForUpdate(string id);
 
     }
 }
